af731c0665e1c3183167f8a773e6b7f99be6923a
Gabriel Abrahao <gabrielmabrahao@gmail.com>
2026-08-28 20:18:21 +0200

    Pad: AR6 LUC emissions

--- pad.R
+++ pad.R
@@ -1,3 +1,61 @@
+require(tidyverse)
+require(quitte)
+
+ar6datafname <- "/mnt/c/pik/nena-klims-analysis/data/data.Rdata"
+ar6data <- load(ar6datafname)
+ar6data[3]
+
+modelfamily <- read.csv2("/mnt/c/pik/nena-klims-analysis/model_family.csv")
+
+ar6_data_world %>% saveRDS("ar6_data_world.rds")
+
+ar6_data_world %>% select(variable) %>% unique %>% write.csv("varlist_ar6_data_world.csv", row.names = FALSE)
+
+ar6emissions <- ar6_data_world %>% 
+  filter(variable %in% c(
+    "Emissions|CO2",
+    "Emissions|CO2|AFOLU",
+    "Emissions|CO2|Energy and Industrial Processes"
+  )) %>%
+    group_by_at(vars(-period, -value)) %>%
+  arrange(period, .by_group = T) %>%
+  mutate(dt = period - lag(period)) %>%
+  filter(period >= 2020) %>%
+  mutate(value = cumsum(value * dt)) %>%
+  mutate(variable = paste0(variable, "|Cum")) %>%
+  select(-dt) %>%
+  ungroup() %>%
+  left_join(modelfamily, by = "model") 
+
+ar6peakbudgets <- ar6emissions %>%
+  filter(variable == "Emissions|CO2|Cum") %>%
+  group_by_at(vars(-period,-value)) %>%
+  filter(value == max(value)) %>%
+  mutate(peakbudget = value*1e-3) %>%
+  ungroup() %>%
+  select(model, scenario, peakbudget) 
+
+options(width = 200)
+ar6emissions <- ar6emissions %>%
+  left_join(ar6peakbudgets) 
+
+ar6emissions %>%
+  filter(period == 2050) %>%
+  filter(variable == "Emissions|CO2|AFOLU|Cum") %>%
+  filter(modfamily == "REMIND-MAgPIE") %>%
+  mutate(value = value*1e-3) %>%
+  ggplot(aes(x = peakbudget, y = value, color = modfamily)) +
+  geom_point() +
+  xlim(0,1000)
+
+ar6emissions %>%
+  filter(variable == "Emissions|CO2|Cum") %>%
+  mutate(value = value*1e-3) %>%
+  filter(model %in% c("REMIND-MAgPIE 2.1-4.3")) %>%
+  ggplot(aes(x = period, y = value, color = modfamily, group = scenario)) +
+  geom_line() 
+
+saveRDS(ar6emissions, "ar6emissions.rds")
 
 inclimmif <- readRDS(climfname)
 
